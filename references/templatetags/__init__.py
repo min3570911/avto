b22b2ab220dfa0c699c40bb6f26cdfc2776e5839
Work <7035911@tut.by>
2025-07-25 12:47:36 +0300

    хочу сделать отдельное приложение

--- references/templatetags/__init__.py
+++
@@ -1,9 +0,0 @@
-# 📁 products/templatetags/__init__.py
-# 🔧 Файл инициализации для template tags Django
-
-# Этот файл нужен для того, чтобы Django распознал директорию
-# products/templatetags как Python пакет с template фильтрами
-
-# 🎯 Без этого файла Django не сможет загрузить фильтры
-# из category_filters.py и будет выдавать ошибку:
-# "Invalid filter: 'smart_truncate_sentences'"
